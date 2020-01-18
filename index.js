706c1e22837655086e4a57c50885b8affa97869d
iPancup <ivanyusuf642@gmail.com>
2020-01-18 22:33:03 +0700

    first

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
+ 
+// parse application/json
+app.use(bodyParser.json());
+ 
+//create database connection
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ebo2ok'
+});
+ 
+//connect to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
+ 
+//tampilkan semua data user
+app.get('/api/user',(req, res) => {
+  let sql = "SELECT * FROM user";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+ 
+//tampilkan data user berdasarkan id
+app.get('/api/user/:id',(req, res) => {
+  let sql = "SELECT * FROM user WHERE user_id="+req.params.id;
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+ 
+//Tambahkan data user baru
+app.post('/api/user',(req, res) => {
+  let data = {username: req.body.username, email: req.body.email, password: req.body.password};
+  let sql = "INSERT INTO user SET ?";
+  let query = conn.query(sql, data,(err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+ 
+//Edit data user berdasarkan id
+app.put('/api/user/:id',(req, res) => {
+  let sql = "UPDATE user SET username='"+req.body.username+"', email='"+req.body.email+"', password='"+req.body.password+"' WHERE user_id="+req.params.id;
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+ 
+//Delete data user berdasarkan id
+app.delete('/api/user/:id',(req, res) => {
+  let sql = "DELETE FROM user WHERE user_id="+req.params.id+"";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});
+ 
+//Server listening
+app.listen(3000,() =>{
+  console.log('Server started on port 3000...');
+});
